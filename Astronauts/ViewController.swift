94877c0c849cc35326362a594590b5081cbc60d1
Cameron Bernhardt <cameronbernhardt@Camerons-iMac.local>
2014-07-16 01:43:52 -0400

    Initial Commit

---
+++ Astronauts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Astronauts
+//
+//  Created by Cameron Bernhardt on 7/16/14.
+//  Copyright (c) 2014 Cameron Bernhardt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
