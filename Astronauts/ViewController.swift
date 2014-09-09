fc922f6585c8550720504bbc1557abf3b2c0157a
Cameron Bernhardt <cameronbernhardt@Camerons-MacBook-Pro.local>
2014-09-09 16:49:15 -0400

    Release 1.0

--- Astronauts/ViewController.swift
+++ Astronauts/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 import iAd
 
 class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDelegate {
+    let height: CGFloat = UIScreen.mainScreen().bounds.size.height //grab screen size
+    let width: CGFloat = UIScreen.mainScreen().bounds.size.width
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let refresh = UIButton.buttonWithType(UIButtonType.System) as UIButton //create refresh button
+        
+        refresh.frame = CGRectMake(110, height/1.23, 100, 50)
+        refresh.titleLabel.font = UIFont(name: "Arial", size: 15)
+        refresh.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
+        refresh.setTitle("Refresh", forState: UIControlState.Normal)
+        refresh.addTarget(self, action: "reload:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(refresh)
+        
         loadData()
+        
         self.canDisplayBannerAds = true
         self.ad.delegate = self
         self.ad.alpha = 0.0
@@ -29,26 +41,24 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
         return NSData(contentsOfURL: NSURL(string: urlToRequest))
     }
     
-    func parseJSON(inputData: NSData) -> NSDictionary{
+    func parseJSON(inputData: NSData) -> NSDictionary {
         var error: NSError?
         let JSON:NSDictionary = NSJSONSerialization.JSONObjectWithData(inputData, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary
         
         return JSON
     }
     
-    @IBOutlet weak var refresh: UIButton!
-    @IBOutlet weak var numPeople: UILabel!
-    @IBOutlet weak var list: UILabel!
-    @IBOutlet weak var descriptionText: UILabel!
-    @IBOutlet weak var ad: ADBannerView!
+    let descriptionText = UILabel()
+    let ad = ADBannerView()
+    let numPeople = UILabel()
+    
     
     var urls:[String] = [] //holds URLs for astronauts
     var ind:Int = 0 //give each astronaut an ID to keep track
     var buttons:[UIButton] = []
     
-    @IBAction func reload(){
+    func reload(sender:UIButton!){
         numPeople.hidden = true
-        list.hidden = true
         descriptionText.hidden = true
         
         for(var i = 0; i < ind; i++){ //remove all of the buttons
@@ -80,19 +90,31 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
     
     func loadData(){
         var request:NSData? = getJSON("http://api.open-notify.org/astros.json")
+        
+        var x:CGFloat = 10, y:CGFloat = height/2.3 //positions
+        
         if(request != nil){
             var data = parseJSON(request!)
             for (key, value) in data {
                 if key as String == "number" {
                     numPeople.hidden = false
-                    list.hidden = false
                     descriptionText.hidden = false
-                    var num:Int = value.integerValue
+                    let num:Int = value.integerValue
+                    
                     numPeople.text = String(num)
-                    list.text = ""
+                    numPeople.textColor = UIColor.whiteColor()
+                    numPeople.font = UIFont(name: "Arial", size: 100)
+                    numPeople.frame = CGRectMake(130, 0, 100, 273)
+                    
+                    descriptionText.textColor = UIColor.whiteColor()
+                    descriptionText.frame = CGRectMake(25, height/2.9, 275, 50)
+                    descriptionText.textAlignment = NSTextAlignment.Center
+                    descriptionText.numberOfLines = 2
                     descriptionText.text = "astronauts are in space.\nThey are:"
+                    
+                    self.view.addSubview(numPeople)
+                    self.view.addSubview(descriptionText)
                 } else if key as String == "people" {
-                    var x:CGFloat = 10, y:CGFloat = 275 //positions
                     for person in value as NSArray {
                         let name = person["name"] as String //name of astronaut
                         let craft = person["craft"] as String //location of astronaut
@@ -114,7 +136,7 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
                         
                         button.tag = ind
                         button.frame = CGRectMake(x, y, 300, 30)
-                        button.titleLabel.font = UIFont(name: "Garamond", size: 15)
+                        button.titleLabel.font = UIFont(name: "Arial", size: 17)
                         button.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
                         button.setTitle("\(name): \(craft)", forState: UIControlState.Normal)
                         button.addTarget(self, action: "labelTapped:", forControlEvents: UIControlEvents.TouchUpInside)
