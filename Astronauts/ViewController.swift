03be9878d232f52dd059ab814e116329d9b8063d
Cameron Bernhardt <cameronbernhardt@Camerons-MacBook-Pro.local>
2014-10-03 14:43:27 -0400

    Updated for iOS 8.1 beta 1

--- Astronauts/ViewController.swift
+++ Astronauts/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
         
         refresh.frame = CGRectMake(110, height/1.23, 100, 50)
         refresh.titleLabel?.font = UIFont(name: "Arial", size: 15)
-        refresh.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
+        refresh.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
         refresh.setTitle("Refresh", forState: UIControlState.Normal)
         refresh.addTarget(self, action: "reload:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(refresh)
@@ -38,13 +38,13 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
     }
     
     func getJSON(urlToRequest: String) -> NSData{
-        return NSData(contentsOfURL: NSURL(string: urlToRequest))
+        return NSData(contentsOfURL: NSURL(string: urlToRequest)!)!
     }
     
     func parseJSON(inputData: NSData) -> NSDictionary? {
         var error: NSError?
         if let JSON:NSDictionary = NSJSONSerialization.JSONObjectWithData(inputData, options: NSJSONReadingOptions.MutableContainers, error: &error) as? NSDictionary {
-                return JSON
+            return JSON
         }
         
         return nil
@@ -55,15 +55,15 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
     let numPeople = UILabel()
     
     
-    var urls:[String] = [] //holds URLs for astronauts
-    var ind:Int = 0 //give each astronaut an ID to keep track
-    var buttons:[UIButton] = []
+    var urls: [String] = [] //holds URLs for astronauts
+    var ind: Int = 0 //give each astronaut an ID to keep track
+    var buttons: [UIButton] = []
     
-    func reload(sender:UIButton!){
+    func reload(sender:UIButton!) {
         numPeople.hidden = true
         descriptionText.hidden = true
         
-        for(var i = 0; i < ind; i++){ //remove all of the buttons
+        for(var i = 0; i < ind; i++) { //remove all of the buttons
             buttons[i].removeFromSuperview()
         }
         
@@ -74,15 +74,15 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
         loadData()
     }
     
-    func labelTapped(sender:UIButton!) { //handles button clicks
+    func labelTapped(sender: UIButton!) { //handles button clicks
         let urlIndex = sender.tag
         if (urlIndex >= 0 && urlIndex < self.urls.count) {
             openUrl(urls[urlIndex])
         }
     }
     
-    func openUrl(url:String!) { //opens URLs
-        let targetURL = NSURL.URLWithString(url)
+    func openUrl(url: String!) { //opens URLs
+        let targetURL = NSURL(fileURLWithPath: url)!
         
         let application = UIApplication.sharedApplication()
         
@@ -91,67 +91,65 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
     }
     
     func loadData(){
-        var request:NSData? = getJSON("http://api.open-notify.org/astros.json")
+        var request: NSData? = getJSON("http://api.open-notify.org/astros.json")
         
         var x:CGFloat = 10, y:CGFloat = height/2.3 //positions
         
         if(request != nil){
             var data = parseJSON(request!)!
-            for (key, value) in data {
-                if key as NSString == "number" {
-                    numPeople.hidden = false
-                    descriptionText.hidden = false
-                    let num:Int = value.integerValue
-                    
-                    numPeople.text = String(num)
-                    numPeople.textColor = UIColor.whiteColor()
-                    numPeople.font = UIFont(name: "Arial", size: 100)
-                    numPeople.frame = CGRectMake(130, 0, 100, 273)
-                    
-                    descriptionText.textColor = UIColor.whiteColor()
-                    descriptionText.frame = CGRectMake(25, height/2.9, 275, 50)
-                    descriptionText.textAlignment = NSTextAlignment.Center
-                    descriptionText.numberOfLines = 2
-                    descriptionText.text = "astronauts are in space.\nThey are:"
-                    
-                    self.view.addSubview(numPeople)
-                    self.view.addSubview(descriptionText)
-                } else if key as NSString == "people" {
-                    for person in (value as NSArray) {
-                        let name: AnyObject? = person["name"]! //name of astronaut
-                        let craft: AnyObject? = person["craft"]! //location of astronaut
-                        
-                        var encodedURL = ""
-                        
-                        for j in "\(name!)" {
-                            if j == " " {
-                                encodedURL = encodedURL + "+"
-                            } else {
-                                encodedURL = encodedURL + (String(j))
-                            }
-                        }
-                        
-                        urls.append("http://google.com/search?q=" + encodedURL)
-                        
-                        //create a button for each astronaut
-                        let button = UIButton.buttonWithType(UIButtonType.System) as UIButton
-                        
-                        button.tag = ind
-                        button.frame = CGRectMake(x, y, 300, 30)
-                        button.titleLabel?.font = UIFont(name: "Arial", size: 17)
-                        button.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
-                        button.setTitle("\(name!): \(craft!)", forState: UIControlState.Normal)
-                        button.addTarget(self, action: "labelTapped:", forControlEvents: UIControlEvents.TouchUpInside)
-                        
-                        buttons.append(button)
-                        
-                        self.view.addSubview(button)
-                        
-                        y += 30 //move each button down
-                        
-                        ind++
+            
+            numPeople.hidden = false
+            descriptionText.hidden = false
+            
+            let num: Int = data.valueForKey("number")!.integerValue
+            let value: AnyObject? = data.valueForKey("people");
+            
+            numPeople.text = String(num)
+            numPeople.textColor = UIColor.whiteColor()
+            numPeople.font = UIFont(name: "Arial", size: 100)
+            numPeople.frame = CGRectMake(130, 0, 100, 273)
+            
+            descriptionText.textColor = UIColor.whiteColor()
+            descriptionText.frame = CGRectMake(25, height/2.9, 275, 50)
+            descriptionText.textAlignment = NSTextAlignment.Center
+            descriptionText.numberOfLines = 2
+            descriptionText.text = "astronauts are in space.\nThey are:"
+            
+            self.view.addSubview(numPeople)
+            self.view.addSubview(descriptionText)
+            for person in (value as NSArray) {
+                let name: AnyObject? = person["name"]! //name of astronaut
+                let craft: AnyObject? = person["craft"]! //location of astronaut
+                
+                var encodedURL = ""
+                
+                for j in "\(name!)" {
+                    if j == " " {
+                        encodedURL = encodedURL + "+"
+                    } else {
+                        encodedURL = encodedURL + (String(j))
                     }
                 }
+                
+                urls.append("http://google.com/search?q=" + encodedURL)
+                
+                //create a button for each astronaut
+                let button = UIButton.buttonWithType(UIButtonType.System) as UIButton
+                
+                button.tag = ind
+                button.frame = CGRectMake(x, y, 300, 30)
+                button.titleLabel?.font = UIFont(name: "Arial", size: 17)
+                button.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
+                button.setTitle("\(name!): \(craft!)", forState: UIControlState.Normal)
+                button.addTarget(self, action: "labelTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+                
+                buttons.append(button)
+                
+                self.view.addSubview(button)
+                
+                y += 30 //move each button down
+                
+                ind++
             }
         }else{
             descriptionText.hidden = false
