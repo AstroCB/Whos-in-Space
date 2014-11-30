1baeb14f6776756c88cf9cc6a42276a483dfbd43
Cameron Bernhardt <cambernhardt@me.com>
2014-11-30 15:55:04 -0500

    Fixed connection bugs, optimized for 6 and 6 Plus screen sizes, and rewrote to use Storyboards properly using knowledge gained from writing AB Day and Key Club
    Release v1.0.1

--- Astronauts/ViewController.swift
+++ Astronauts/ViewController.swift
@@ -9,19 +9,23 @@
 import UIKit
 import iAd
 
-class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDelegate {
-    let height: CGFloat = UIScreen.mainScreen().bounds.size.height //grab screen size
+class ViewController: UIViewController, ADBannerViewDelegate {
+    let height: CGFloat = UIScreen.mainScreen().bounds.size.height // Grab screen size
     let width: CGFloat = UIScreen.mainScreen().bounds.size.width
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        descriptionText.text = "astronauts are in space. They are:"
-        loadData()
         
-        self.canDisplayBannerAds = true
-        self.ad.delegate = self
-        self.ad.alpha = 0.0
+        // iAd setup
+        if height <= 480 {
+            self.canDisplayBannerAds = false
+        } else {
+            self.canDisplayBannerAds = true
+        }
+        
+        // Initial load
+        loadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -29,8 +33,8 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
         // Dispose of any resources that can be recreated.
     }
     
-    func getJSON(urlToRequest: String) -> NSData{
-        return NSData(contentsOfURL: NSURL(string: urlToRequest)!)!
+    func getJSON(urlToRequest: String) -> NSData? {
+        return NSData(contentsOfURL: NSURL(string: urlToRequest)!)
     }
     
     func parseJSON(inputData: NSData) -> NSDictionary? {
@@ -42,22 +46,22 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
         return nil
     }
     
-
-    let ad = ADBannerView()
     @IBOutlet weak var numPeople: UILabel!
     @IBOutlet weak var descriptionText: UILabel!
+    @IBOutlet weak var trailText: UILabel!
+    @IBOutlet weak var ad: ADBannerView!
     
     
-    var urls: [String] = [] //holds URLs for astronauts
-    var ind: Int = 0 //give each astronaut an ID to keep track
+    var urls: [String] = [] // Holds URLs for astronauts
+    var ind: Int = 0 // Give each astronaut an ID to keep track
     var buttons: [UIButton] = []
     
     @IBAction func reload(sender:UIButton!) {
         numPeople.hidden = true
         descriptionText.hidden = true
         
-        for(var i = 0; i < ind; i++) { //remove all of the buttons
-            buttons[i].removeFromSuperview()
+        for i in buttons { // Remove all of the buttons
+            i.removeFromSuperview()
         }
         
         buttons = []
@@ -67,72 +71,127 @@ class ViewController: UIViewController, NSURLConnectionDelegate, ADBannerViewDel
         loadData()
     }
     
-    func labelTapped(sender: UIButton!) { //handles button clicks
+    func labelTapped(sender: UIButton!) { // Handles button clicks
         let urlIndex = sender.tag
-        if (urlIndex >= 0 && urlIndex < self.urls.count) {
-            openUrl(urls[urlIndex])
-        }
+        self.openUrl(urls[urlIndex])
     }
     
-    func openUrl(url: String!) { //opens URLs
-        let targetURL = NSURL(fileURLWithPath: url)!
+    func openUrl(url: String!) { // Opens URLs
+        let targetURL = NSURL(string: url)!
         let application = UIApplication.sharedApplication()
         
-        application.openURL(targetURL);
+        application.openURL(targetURL)
         
     }
     
     func loadData(){
-        var request: NSData? = getJSON("http://api.open-notify.org/astros.json")
+        var dist: CGFloat = 0
+        // Multipliers for constraint positioning later
+        var constMultiplier: CGFloat = 1
+        var topConst: CGFloat = 1.1
         
-        var x:CGFloat = height/15.0, y:CGFloat = height/2.3 //positions
+        // Fiddle with positioning based on device size; this is bad style, but I don't know a better way
+        switch height {
+        case 480: // 4s
+            dist = 25
+            constMultiplier = 1.03
+            topConst = 1.05
+        case 568: // 5/s
+            dist = 30
+            constMultiplier = 1.05
+        case 667: // 6
+            dist = 50
+            constMultiplier = 1.075
+        case 736: // 6 Plus
+            dist = 70
+            constMultiplier = 1.1
+        default: // ??
+            dist = 30
+            constMultiplier = 1.05
+        }
         
-        if(request != nil){
-            var data = parseJSON(request!)!
-            
+        if let request: NSData = getJSON("http://api.open-notify.org/astros.json") {
+            let data: NSDictionary = parseJSON(request)!
             numPeople.hidden = false
             descriptionText.hidden = false
+            trailText.hidden = false
             
-            let num: Int = data.valueForKey("number")!.integerValue
-            let value: AnyObject? = data.valueForKey("people");
-            
-            for person in (value as NSArray) {
-                let name: AnyObject? = person["name"]! //name of astronaut
-                let craft: AnyObject? = person["craft"]! //location of astronaut
-                
-                var encodedURL = ""
-                
-                for j in "\(name!)" {
-                    if j == " " {
-                        encodedURL = encodedURL + "+"
-                    } else {
-                        encodedURL = encodedURL + (String(j))
-                    }
-                }
-                
-                urls.append("http://google.com/search?q=" + encodedURL)
-                
-                //create a button for each astronaut
-                let button = UIButton.buttonWithType(UIButtonType.System) as UIButton
-                
-                button.tag = ind
-                button.frame = CGRectMake(x, y, 300, 30)
-                button.titleLabel?.font = UIFont(name: "Arial", size: 17)
-                button.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
-                button.setTitle("\(name!): \(craft!)", forState: UIControlState.Normal)
-                button.addTarget(self, action: "labelTapped:", forControlEvents: UIControlEvents.TouchUpInside)
-                
-                buttons.append(button)
-                
-                self.view.addSubview(button)
-                
-                y += 30 //move each button down
-                
-                ind++
+            if let num: Int = data.valueForKey("number")?.integerValue {
+                numPeople.text = "\(num)"
             }
-        }else{
-            descriptionText.hidden = false
-            descriptionText.text = "Data currently unavailable.\nCheck your network connection."
+            
+            if let value: NSArray = data.valueForKey("people") as? NSArray {
+                for person in value {
+                    let name: String = person["name"] as String // Name of astronaut
+                    let craft: String = person["craft"] as String // Location of astronaut
+                    
+                    var encodedURL = ""
+                    
+                    for j in name {
+                        if j == " " {
+                            encodedURL = encodedURL + "+"
+                        } else {
+                            encodedURL = encodedURL + (String(j))
+                        }
+                    }
+                    
+                    urls.append("http://google.com/search?q=" + encodedURL)
+                    
+                    // Create a button for each astronaut
+                    let button = UIButton.buttonWithType(UIButtonType.System) as UIButton
+                    
+                    button.tag = ind
+                    button.titleLabel?.font = UIFont(name: "Arial", size: 17)
+                    button.setTitleColor(UIColor.whiteColor(), forState:UIControlState.Normal)
+                    button.setTitle("\(name): \(craft)", forState: UIControlState.Normal)
+                    button.addTarget(self, action: "labelTapped:", forControlEvents: UIControlEvents.TouchUpInside)
+                    
+                    buttons.append(button)
+                    
+                    self.view.addSubview(button)
+                    
+                    // Positioning and constraints for buttons
+                    button.setTranslatesAutoresizingMaskIntoConstraints(false)
+                    
+                    let constX = NSLayoutConstraint(item: button, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0)
+                    self.view.addConstraint(constX)
+                    
+                    if ind == 0 {
+                        let constY = NSLayoutConstraint(item: button, attribute: .Top, relatedBy: .Equal, toItem: self.trailText, attribute: .CenterY, multiplier: topConst, constant: 0)
+                        self.view.addConstraint(constY)
+                    } else {
+                        let lastButton: UIButton = self.buttons[ind - 1]
+                        let constY = NSLayoutConstraint(item: button, attribute: .Top, relatedBy: .Equal, toItem: lastButton, attribute: .CenterY, multiplier: constMultiplier, constant: 0)
+                        self.view.addConstraint(constY)
+                    }
+                    
+                    ind++
+                }
+            }
+        }else{ // No connection...or the API has been taken down, which would be bad (and possibly my fault)
+            numPeople.hidden = true
+            descriptionText.hidden = true
+            trailText.hidden = true
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                let version: NSString = UIDevice.currentDevice().systemVersion as NSString
+                if  version.doubleValue >= 8 {
+                    let alert: UIAlertController = UIAlertController(title: "Data currently unavailable.", message: "Check your network connection.", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                    
+                } else {
+                    let alert: UIAlertView = UIAlertView()
+                    alert.delegate = self
+                    
+                    alert.title = "Data currently unavailable."
+                    alert.message = "Check your network connection."
+                    alert.addButtonWithTitle("OK")
+                    
+                    alert.show()
+                }
+            })
+            
         }
     }
 }
